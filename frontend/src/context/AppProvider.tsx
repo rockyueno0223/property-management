8ca3fe54b29d55a4a34e952a24d73cc94866389a
Hiroki Ueno <rockyueno@gmail.com>
2024-11-15 13:36:51 -0800

    persisted user in app provider

--- frontend/src/context/AppProvider.tsx
+++ frontend/src/context/AppProvider.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppContext } from './AppContext';
 import { User } from '../../../shared/types/user';
 import { Property } from '../../../shared/types/property';
 
 export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<User | null>(() => {
+    const savedUser = sessionStorage.getItem('user');
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
   const [properties, setProperties] = useState<Property[]>([]);
+
+  useEffect(() => {
+    if (user) {
+      sessionStorage.setItem('user', JSON.stringify(user));
+    } else {
+      sessionStorage.removeItem('user');
+    }
+  }, [user])
 
   return (
     <AppContext.Provider value={{ user, setUser, properties, setProperties }}>
